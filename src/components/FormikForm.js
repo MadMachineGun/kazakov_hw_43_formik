af58328f99cfe86cc0b6f8c5bbb08a3174a4b9bc
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-17 09:20:14 +0200

    start

--- src/components/FormikForm.js
+++ src/components/FormikForm.js
@@ -1,27 +1,50 @@
-import {Form, Field, Formik} from "formik";
+import React from 'react';
+import { Form, Field, Formik } from 'formik';
 import './formik-form.scss';
 
 export default function FormikForm() {
     const submitForm = (values) => {
         console.log(values);
-    }
+    };
+
+    const validateEmail = (value) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return !value || emailRegex.test(value)
+            ? undefined
+            : 'Некоректний формат email';
+    };
+
+    const validatePhone = (value) => {
+        const phoneRegex = /^\d{12}$/;
+        return !value || phoneRegex.test(value)
+            ? undefined
+            : 'Тільки цифри та довжина 12';
+    };
+
     return (
         <>
-            <Formik
-                initialValues={{
-                    firstName: '',
-                    surName: '',
-                }}
-                onSubmit={submitForm}>
-                <Form className='form'>
-                    <span>First Name</span>
-                    <Field type='text' name='firstName'/>
-                    <span>Surname</span>
-                    <Field type='text' name='surtName'/>
-                    <button type='submit'>Sign in</button>
-
-                </Form>
-            </Formik>
+            <div className='form'>
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        surName: '',
+                        eMail: '',
+                        telNumber: '',
+                    }}
+                    onSubmit={submitForm}>
+                    <Form className='form'>
+                        <span>First Name</span>
+                        <Field type='text' name='firstName' />
+                        <span>Surname</span>
+                        <Field type='text' name='surName' />
+                        <span>e-mail</span>
+                        <Field type='text' name='eMail' validate={validateEmail} />
+                        <span>Tel.</span>
+                        <Field type='text' name='telNumber' validate={validatePhone} />
+                        <button type='submit'>Sign in</button>
+                    </Form>
+                </Formik>
+            </div>
         </>
     );
 }
